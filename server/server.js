9ebe156216a7546a2a73e769153ba09d42e89e50
stephan231 <francisstephan.05@gmail.com>
2025-07-08 01:34:11 +0530

    Added db url

--- server/server.js
+++ server/server.js
@@ -19,7 +19,9 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect("db_url")
+  .connect(
+    "mongodb+srv://stephanf2324:uOFuQ6ESwy24IFpy@cluster0.8wiagov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
